069a92219510c00584aba7902d81d431d81c7af5
1NF1NIT3 <103411606+1NF1NIT3@users.noreply.github.com>
2025-11-01 21:53:14 +0800

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'music-widget-cache-v5';
+const CACHE_NAME = 'music-widget-cache-v6';
 const urlsToCache = [
     './',
     './index.html',
@@ -82,6 +82,7 @@ self.addEventListener('fetch', event => {
             })
     );
 });
+
 
 
 
